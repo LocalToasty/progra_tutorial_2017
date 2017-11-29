842ab5d6db0d56e9d667db2fa65be44baf353473
Marko van Treeck <markovantreeck@gmail.com>
2017-11-29 06:25:49 -0500

    Add skeleton for Task 6.1

---
+++ ex6/List.java
@@ -0,0 +1,22 @@
+public class List {
+  public List(List n, int v) {
+  }
+
+  public List getNext() {
+  }
+
+  public int getValue() {
+  }
+
+  public boolean isEmpty() {
+  }
+
+  public String toString() {
+  }
+
+  public int length() {
+  }
+
+  public List getSublist(int length) {
+  }
+}
